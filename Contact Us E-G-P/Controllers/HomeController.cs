dfdf2d60c6f6f3f8b9b7741fef9db8ce6821f4e7
prana <prana@PRANAV>
2024-01-13 19:43:40 +0530

    Add project files.

---
+++ Contact Us E-G-P/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contact_Us_E_G_P.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Contact Us E-G-P/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Contact_Us_E_G_P.MvcApplication" Language="C#" %>
